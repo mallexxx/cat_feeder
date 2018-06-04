44e61979b97024fb723571d61f387eb80d2179b8
Alexey Martemyanov <mallexxx@gmail.com>
2018-06-04 12:10:27 +0700

    Updated for TMC2130 stepper driver; Feed by Button; Time config from Serial

--- cat_feeder_sketch/cat_feeder_sketch.ino
+++ cat_feeder_sketch/cat_feeder_sketch.ino
@@ -1,16 +1,26 @@
 #include "U8glib.h"
 #include <Wire.h>
+#include <TMC2130Stepper.h>
 
 #define RTC_I2C_ADDRESS 0x68
 U8GLIB_SSD1306_128X64 u8g(U8G_I2C_OPT_NONE);
 
-#define DIR 5
-#define STEP 6
+#define BUTTON 5
+#define CS 6
 #define ENA 7
+#define DIR 8
+#define STEP 9
 
-#define LED 13
+TMC2130Stepper driver = TMC2130Stepper(ENA, DIR, STEP, CS);
 
-#define ENTER 2
+uint8_t second = 0;
+uint8_t minute = 0;
+uint8_t hour = 0;
+int16_t togo = 1;
+uint16_t counter = 0;
+uint8_t cnt = 0;
+uint8_t data= 0;
+char cmd;
 
 void setTime(byte second, byte minute, byte hour, byte dayOfWeek, byte dayOfMonth, byte month, byte year) {
   // sets time and date data to DS3231
@@ -26,27 +36,21 @@ void setTime(byte second, byte minute, byte hour, byte dayOfWeek, byte dayOfMont
   Wire.endTransmission();
 }
 
-void setup() { 
-  Wire.begin();
-    
-  pinMode(ENA, OUTPUT); 
-  pinMode(DIR, OUTPUT); 
-  pinMode(STEP, OUTPUT);
-  digitalWrite(DIR, LOW);
-  digitalWrite(STEP, LOW);
+void setup() {
   digitalWrite(ENA, HIGH);
   
-  pinMode(LED, OUTPUT);
-  pinMode(ENTER, INPUT);
+  Serial.begin(115200);
+  Serial.print("Starting...");
+  
+  Wire.begin();
+    
+  pinMode(BUTTON, INPUT);
+  
+  driver.begin();
+  driver.rms_current(1300);
+  driver.stealthChop(1);
 
-  for (int i = 0; i < 100; i++) {
-    delay (5);
-    digitalWrite(DIR, LOW);
-    digitalWrite(STEP, LOW);
-    digitalWrite(ENA, HIGH);
-  }
-
-//  setTime(0, 34, 16, 5, 2, 9, 2017);
+  Serial.println(" Ready!");
 } 
 
 inline uint8_t bcd2dec(uint8_t bcd) {
@@ -56,14 +60,8 @@ inline uint8_t decToBcd(byte val) {
   return( (val/10*16) + (val%10) );
 }
 
-uint8_t second = 0;
-uint8_t minute = 0;
-uint8_t hour = 0;
-int16_t togo = 1;
-uint16_t counter = 0;
-
 void draw() {
-  u8g.setRot180();
+//  u8g.setRot180();
   u8g.setFont(u8g_font_unifont);
   u8g.setPrintPos(10, 20);
   u8g.print(hour, DEC);
@@ -92,26 +90,28 @@ void draw() {
   }
 }
 
-void feed() {
-  digitalWrite(ENA, LOW);
+void feed(uint8_t cnt) {
+  Serial.print("Feeding (");
+  Serial.print(cnt, DEC);
+  Serial.println(")");
   
-  for(uint8_t i=0; i < 5; i++){   
-    for (uint8_t f=0 ; f<90; f++) {
-      digitalWrite(DIR, LOW);
-            
+  digitalWrite(ENA, LOW);
+
+  for(uint8_t i=0; i < cnt; i++){
+    driver.shaft_dir(0);
+    for (uint16_t f=0 ; f<9000; f++) {
+      digitalWrite(STEP, HIGH);
+      delayMicroseconds(5);
       digitalWrite(STEP, LOW);
-      delay(5);
-      
-      digitalWrite(STEP, HIGH);
-      delay(5);
+      delayMicroseconds(5);
     }
-    for (uint8_t f=0 ; f<60; f++) {
-      digitalWrite(DIR, HIGH);
-       
-      digitalWrite(STEP, LOW);    
-      delay(5);
+    driver.shaft_dir(1);
+    for (uint16_t f=0 ; f<16000; f++) {
       digitalWrite(STEP, HIGH);
-      delay(5);
+      delayMicroseconds(5);
+
+      digitalWrite(STEP, LOW);
+      delayMicroseconds(5);
     }
   }
   digitalWrite(ENA, HIGH);
@@ -128,6 +128,106 @@ void loop() {
     minute = bcd2dec( Wire.read() );
     hour = bcd2dec( Wire.read() );
 
+    if (Serial.available()) {
+      data = Serial.read();
+      cmd = data;
+      
+      if (Serial.available()) {
+        data = Serial.read();
+        
+        if ('0' <= data && data <= '9') {
+          cnt = data - '0';
+          
+          if (Serial.available()) {
+            data = Serial.read();
+
+            if ('0' <= data && data <= '9') {
+              cnt = cnt * 10;
+              cnt += data - '0';
+            }
+          }
+        } else {
+          cnt = 1;
+        }
+      } else {
+        cnt = 1;
+      }
+      switch (cmd) {
+      case 'f':
+        // Feed:
+        feed(cnt);
+        break;
+        
+      case 'm':
+        if (cnt <= 60) {
+          // Set minute
+          minute = cnt;
+          Serial.print("Setting minutes to ");
+          Serial.println(minute, DEC);
+          cnt=255;
+        } else {
+          break;
+        }
+        
+        // fallthrough
+      case 'h':
+        if (cnt <= 24) {
+          // Set hour
+          hour = cnt;
+          Serial.print("Setting hours to ");
+          Serial.println(hour, DEC);
+          cnt=255;
+        }// else {
+          // fallthrough
+        //}
+        
+      case 's':
+        if (cnt <= 60) {
+          // Set second
+          second = cnt;
+          Serial.print("Setting seconds to ");
+          Serial.println(second, DEC);
+          cnt=255;
+        }
+      
+        // Update time
+        setTime(second, minute, hour, 7, 2, 6, 18);
+        
+      // fallthrough
+        
+      case 't':
+        Serial.print("The current time is: ");
+        Serial.print(hour, DEC);
+        Serial.print(":");
+        if (minute < 10)
+          Serial.print("0");
+        Serial.print(minute, DEC);
+        Serial.print(":");
+        if (second < 10)
+          Serial.print("0");
+        Serial.println(second, DEC);
+        break;
+        
+      }
+      cnt = 0;
+    }
+    if (digitalRead(BUTTON)) {
+        if (!cnt) {
+            cnt = 1;
+            counter++;
+
+            togo = 0;
+            u8g.firstPage();  
+            do {
+              draw();
+            } while( u8g.nextPage() );
+            
+            feed(1);
+        }
+    } else {
+        cnt = 0;
+    }
+
     // 8:00 = 480min
     // 17:00 = 1020min
     // 23:00 = 1380min
@@ -139,8 +239,10 @@ void loop() {
       draw();
     } while( u8g.nextPage() );
 
-    if (second == 0 && togo == 0)
-      feed();
+    if (second == 0 && togo == 0) {
+        counter++;
+        feed(16);
+    }
     delay(250);
 }
   
